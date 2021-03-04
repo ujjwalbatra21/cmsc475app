8da9bcab851fc0f1491411032b2b7e7794df2ba7
Kameron Bryant <bryantkp@vcu.edu>
2021-03-04 17:32:30 -0500

    Edited New game activity

--- app/src/main/java/com/example/cmsc475app/NewGame.java
+++ app/src/main/java/com/example/cmsc475app/NewGame.java
@@ -3,6 +3,8 @@ package com.example.cmsc475app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class NewGame extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class NewGame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game);
+
+        Spinner spinner  = findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Numbers, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
     }
 }
