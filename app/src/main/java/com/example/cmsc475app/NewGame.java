24c655be4636171bb4028fb744b238e372d2ff64
Kameron Bryant <bryantkp@vcu.edu>
2021-03-04 17:59:32 -0500

    Drop down player spinner created( New game activity)

--- app/src/main/java/com/example/cmsc475app/NewGame.java
+++ app/src/main/java/com/example/cmsc475app/NewGame.java
@@ -3,10 +3,13 @@ package com.example.cmsc475app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class NewGame extends AppCompatActivity {
+public class NewGame extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +19,20 @@ public class NewGame extends AppCompatActivity {
         Spinner spinner  = findViewById(R.id.spinner1);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Numbers, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter((adapter));
+        spinner.setOnItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String text = adapterView.getItemAtPosition(i).toString(); // take item at position turn into string store in text
+        Toast.makeText(adapterView.getContext(), text, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
