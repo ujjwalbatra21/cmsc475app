35c27bd3d33eadcd289cae9f3cd13a014bbea6d3
naodm <mogesn@vcu.edu>
2021-03-04 16:17:17 -0500

    updated players menu xml

--- app/src/main/java/com/example/cmsc475app/PlayersScreen.java
+++ app/src/main/java/com/example/cmsc475app/PlayersScreen.java
@@ -12,3 +12,24 @@ public class PlayersScreen extends AppCompatActivity {
         setContentView(R.layout.activity_players_screen);
     }
 }
+
+//public class Player{
+//    public String playerName;
+/*    public int[] par;
+    public int[] over_under;
+    public int totalScore, totalPar, totalOverUnder;
+    public int[] score;
+}
+over_under[i] = par[i]-score[i];
+for(int i = 0; i<19; i++){
+    totalScore +=score[i];
+}
+
+for(int i = 0; i<19; i++){
+    totalPar +=par[i];
+}
+
+for(int i = 0; i<19; i++){
+    totalOverUnder += over_under[i];
+}
+// every entry on the table per hole will be appended to the score array*/
