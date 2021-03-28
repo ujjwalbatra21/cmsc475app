a245e80c2fe2839767f38a86294f7e4922709854
Kameron Bryant <bryantkp@vcu.edu>
2021-03-28 13:27:10 -0400

    Restart on new game activity

--- app/src/main/java/com/example/cmsc475app/NewGame.java
+++ app/src/main/java/com/example/cmsc475app/NewGame.java
@@ -10,8 +10,8 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
-
-public class NewGame extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+//public class NewGame extends AppCompatActivity implements AdapterView.OnItemSelectedListener
+public class NewGame extends AppCompatActivity{
     private Button button;
     private Button button2;
     @Override
@@ -19,11 +19,7 @@ public class NewGame extends AppCompatActivity implements AdapterView.OnItemSele
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game);
 
-        Spinner spinner  = findViewById(R.id.spinner1);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Numbers, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter((adapter));
-        spinner.setOnItemSelectedListener(this);
+
 
 
         button = (Button) findViewById(R.id.button);
@@ -54,15 +50,6 @@ public class NewGame extends AppCompatActivity implements AdapterView.OnItemSele
         startActivity(intent);
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        String text = adapterView.getItemAtPosition(i).toString(); // take item at position turn into string store in text
-        Toast.makeText(adapterView.getContext(), text, Toast.LENGTH_SHORT).show();
 
-    }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
-
-    }
 }
