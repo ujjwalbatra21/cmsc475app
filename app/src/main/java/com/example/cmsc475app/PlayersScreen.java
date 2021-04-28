aae4304af616fd189c26f673c98e546f2bb2eb60
naodm <mogesn@vcu.edu>
2021-04-28 11:07:01 -0400

    new file

--- app/src/main/java/com/example/cmsc475app/PlayersScreen.java
+++ app/src/main/java/com/example/cmsc475app/PlayersScreen.java
@@ -2,6 +2,7 @@ package com.example.cmsc475app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
